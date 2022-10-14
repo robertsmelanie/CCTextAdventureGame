a24fcfaad202a2c5828aa3b4e5ebf12bdc888e5f
Melanie Roberts <robertsmelaniecc@gmail.com>
2022-10-14 18:27:41 -0400

    Update text

--- script.js
+++ script.js
@@ -4,33 +4,33 @@ const enter = "Please enter 1 or 2 for your answer";
 
 const gameOver = "Sorry GAME OVER!";
 
-const start = `Welcome to the Ghostbusters Text Adventure Game. Click 'OK' to start the game.`;
+const start = `Welcome to the Scream Text Adventure Game. Click 'OK' to start the game.`;
 
-const q1 = `You are trying to catch a ghost called "Slimer" at the Sedgwick Hotel but have not seen him yet. Should you...
-1. Take the elevator to look on the 12th floor
+const q1 = `You are trying to survive a killer. It is called ghost face or sometimes just the killer. Unlike other stories it is not a supernatural monster it is just someone or someones in a costume trying to kill you and other people you know. Should you...
+1. Leave school by yourself
 OR
-2. Wait in the lobby to see if he shows up there
+2. Wait for a friend to drive you home
 ${enter}`;
 
 const gameOver1 = `You never see Slimer so you give up on catching ghosts and become a talk show host instead. ${gameOver}`;
 
-const q2 = `When you get off the elevator something startles you! Should you...
-1. Look to see what it is
+const q2 = `You arrive home safely. Then you get a phone call. You answer and it is a wrong number. You hang up and he immediently calls again, Should you...
+1. Keep talking to him he sounds cute
 OR
-2. Shoot at it immediately with your proton pack
+2. Hang up and call the police you can't be too careful with a killer on the loose
 ${enter}`;
 
 const gameOver2 = `It was the hotel maid and you nearly killed her! You are forced to leave the hotel. ${gameOver}`;
 
-const q3 = `It was just the hotel maid. So you walk around to look for Slimer and You Spot Him! Should you...
-1. Call to let the other Ghostbusters know you found him
+const q3 = `The police arrive and they tell you they saw someone run off but lost them. Glad you called the police. They are leaving a couple of cops at your house tonight. You go to school and Billy your boyfriend stops you and says he needs to talk to you alone.k for Slimer and You Spot Him! Should you...
+1. Say OK and Billy says you should leave school to go to a private place
 OR
-2. Hide and wait to see where he goes
+2. Tell Billy you will talk to him after class
 ${enter}`;
 
 const gameOver3 = `Slimer hears you, immediately flies at you, and YOU GET SLIMED! ${gameOver}`;
 
-const q4 = `You see that Slimer goes into the ballroom so you quickly tell the other Ghostbusters and you all head in. You see him flying around when you walk in. Should you...
+const q4 = `You meet Billy after class and he complains that you haven't been the same since your mother died and he wants girlfriend back.  that Slimer goes into the ballroom so you quickly tell the other Ghostbusters and you all head in. You see him flying around when you walk in. Should you...
 1. Shoot at him with your proton pack
 OR
 2. Put two proton packs together to try and create a more powerful shot (A.K.A. Cross The Streams)
@@ -38,15 +38,15 @@ ${enter}`;
 
 const gameOver4 = `All life as you know it stops instantaneously and every molecule in your body explodes at the speed of light. ${gameOver}`;
 
-const q5 = `You wrangled Slimer with your shot and the ghost trap is set underneath him. You noticed there is a bright light coming from the trap and are tempted to look at it. Should you...
-1. Look at the trap
+const q5 = `Biily goes into a rage and pulls out a bloody knife. You now know that he is the killer you panic and run coming from the trap and are tempted to look at it. Should you...
+1. Run to the room at the end of the hall to hide
 OR
-2. Keep your focus on Slimer
+2. Run out the exit door of the building
 ${enter}`;
 
-const gameOver5 = `You looked at the trap and the light BLINDED YOU! ${gameOver}`;
+const gameOver5 = `You run to the room at the end of the hall. There is no way out and nothing to hide behind. Billy kills YOU! ${gameOver}`;
 
-const win = `The ghost trap sucked Slimer in and closed. CONGRATULATIONS! YOU CAUGHT SLIMER!!!`;
+const win = `You run out of the building. You then run past a group of cops that see you and Billy. They arrest Billy throwing him to the ground and take the knife in as evidence. CONGRATULATIONS! YOU SURVIVED!!!`;
 
 
 // Alerts the user to start the game
